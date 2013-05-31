ab7af456f6ea065a263d6b169731ce9fee4662c3
jmiramant <josh@miramant.me>
2013-05-31 13:12:44 -0700

    stuff

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,22 @@
 class Task < ActiveRecord::Base
+
+  def add(args)
+    Task.create(args)
+  end
+
+  def delete(task_num)
+    Task.destroy(task_num)
+  end
+
+  def list
+    Task.all
+  end
+
+  def complete!(task_num)
+    task = Task.find(task_num)
+    task.complete = true
+    task.save
+  end
 end
+
+
--- todo.rb
+++ todo.rb
@@ -1,3 +1,10 @@
 require_relative 'config/application'
 
 puts "Put your application code in #{File.expand_path(__FILE__)}"
+
+input = gets.chomp
+
+input = 'add do this task'
+if task.methods.include?(arg.to_sym)
+  task.send(arg.to_sym(args))
+end
